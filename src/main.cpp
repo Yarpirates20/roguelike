e0d7e9d935a360400d783604566a47da78b40d9b
Robert Samoraj <rsamoraj11@gmail.com>
2026-01-10 19:29:45 -0600

    Flatten directory structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
+#include <ncurses.h>
+
+
 int main()
 {
+    initscr();
+    cbreak();
+    noecho();
+    curs_set(0);
+    keypad(stdscr, TRUE);
+
+    printw("Roguelike Init - Press any key to exit.");
+    refresh();
+
+    getch();
+
+    endwin();
+
     return 0;
 }
